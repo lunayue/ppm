35d5bc184799915078635f4eb424b6b4edb744ae
AngeloNNGGAA <amendona6@gmail.com>
2020-11-05 17:20:16 +0000

    By Gello

--- src/menu.scala
+++ src/menu.scala
@@ -1,15 +1,29 @@
 import scala.io.StdIn.readLine
 
 object menu extends App{
+
   def loginLoop(): Unit ={
     println("(l)ogin, (c)reate user, (q)uit:")
     val input = readLine.trim.toUpperCase
     input match{
-      case "L" => println("login")
-      case "C" => println("create user")
-      case "Q" => println("quit")
-      case _ => println("Invalid Input")
+      case "L" => {
+        println("login")
+        loginLoop()
+      }
+      case "C" => {
+        println("create user")
+        loginLoop()
+      }
+      case "Q" => {
+        println("quit")
+        loginLoop()
+      }
+      case _ => {
+        println("Invalid Input")
+        loginLoop()
+      }
     }
   }
+
   loginLoop()
 }
