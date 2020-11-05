a0e633bf93b04757d9b5853df4bf7e705c5a2802
Syhko <syhko@outlook.com>
2020-11-05 15:54:08 +0000

    syhko

--- src/teste/primeiro.scala
+++ src/teste/primeiro.scala
@@ -5,9 +5,5 @@ class primeiro {
 }
 
 object Hello{
-<<<<<<< HEAD
-  def main(args: Array[String]) = println("Hello World! ai mae")
-=======
-  def main(args: Array[String]) = println("Hello World! ai mae")
->>>>>>> Olá a seguir ao Hello World
+  def main(args: Array[String]) = println("Hello World! syhko")
 }
