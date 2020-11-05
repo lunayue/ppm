bfab6dacaaff080424556fc9590b55f5a3da8344
Syhko <syhko@outlook.com>
2020-11-05 15:46:06 +0000

    idk

--- src/teste/primeiro.scala
+++ src/teste/primeiro.scala
@@ -5,5 +5,9 @@ class primeiro {
 }
 
 object Hello{
+<<<<<<< HEAD
   def main(args: Array[String]) = println("Hello World! Pls don't kill me\n\n\n")
+=======
+  def main(args: Array[String]) = println("Hello World! i dont even loooooooool")
+>>>>>>> Olá a seguir ao Hello World
 }
