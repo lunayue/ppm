fb660ae716b929f77a435f503b976cc265831f1c
Luna Yue <susana_polido@hotmail.com>
2020-11-05 17:41:26 +0000

    Update menu.scala

--- src/menu.scala
+++ src/menu.scala
@@ -14,4 +14,14 @@ object menu extends App{
     }
   }
   loginLoop()
+  
+  def mainLoop(u: String) = {
+    val input = getUserInput("(l)ogin, (c)reate user, (q)uit:")
+    input match{
+      case "L" => login()
+      case "C" => createUser()
+      case "Q" => logout()
+      case _ => println("Invalid Input"); loginLoop()
+    }
+  }
 }
