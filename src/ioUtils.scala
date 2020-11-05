a8c475d73ce202bbc77ab9c89a8468cd4ecff026
AngeloNNGGAA <amendona6@gmail.com>
2020-11-05 18:02:38 +0000

    By Gello

--- src/ioUtils.scala
+++ src/ioUtils.scala
@@ -2,13 +2,13 @@ import scala.io.StdIn.readLine
 
 object ioUtils {
   def getUserInput(pedido: String):String = {
-    println(pedido)
+    print(pedido + ": ")
     val input = readLine.trim.toUpperCase
     input
   }
 
   def getUserInputSensitive(pedido: String):String = {
-    println(pedido)
+    print(pedido+ ": ")
     val input = readLine.trim
     input
   }
--- src/menu.scala
+++ src/menu.scala
@@ -1,8 +1,6 @@
 import ioUtils._
 import session._
 
-import scala.io.StdIn.readLine
-
 object menu extends App{
   def loginLoop(): Unit ={
     val input = getUserInput("(l)ogin, (c)reate user, (q)uit:")
