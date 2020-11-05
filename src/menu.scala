a226ff7000ec3fd95db36bf858b08e3d9b088465
Luna Yue <susana_polido@hotmail.com>
2020-11-05 17:49:28 +0000

    trying to login

--- src/menu.scala
+++ src/menu.scala
@@ -7,8 +7,8 @@ object menu extends App{
   def loginLoop(): Unit ={
     val input = getUserInput("(l)ogin, (c)reate user, (q)uit:")
     input match{
-      case "L" => login()
-      case "C" => createUser()
+      case "L" => login() //case None => loginLoop()
+      case "C" => createUser() //case None => loginLoop()
       case "Q" => exit()
       case _ => println("Invalid Input"); loginLoop()
     }
@@ -16,7 +16,8 @@ object menu extends App{
   loginLoop()
 
   def mainLoop(u: String) = {
-    val input = getUserInput("(l)ogin, (c)reate user, (q)uit:")
+    //criar um objecto como o prof fez para se poder fazer um loop em vez de um long String
+    val input = getUserInput("(l)ogin, (c)reate user, logout")
     input match{
       case "L" => login()
       case "C" => createUser()
--- src/session.scala
+++ src/session.scala
@@ -4,16 +4,18 @@ object session {
   def login() = {
     val username = getUserInputSensitive("Username")
     val password = getUserInputSensitive("Password")
+    //manda Some(User)
   }
 
   def createUser() = {
     val username = getUserInputSensitive("Username")
     val password = getUserInputSensitive("Password")
     val repeticao = getUserInputSensitive("Repetir Password")
+    //manda Some(User)
   }
 
   def logout() ={
-
+    //save para file???
   }
 
   def exit() ={
