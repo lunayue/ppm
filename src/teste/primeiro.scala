291fae3f2a58e5fa59f1331be3b1f7175cbfbc19
Luna Yue <susana_polido@hotmail.com>
2020-11-05 16:22:28 +0000

    Update primeiro.scala

--- src/teste/primeiro.scala
+++ src/teste/primeiro.scala
@@ -6,8 +6,12 @@ class primeiro {
 
 object Hello{
 <<<<<<< HEAD
+<<<<<<< HEAD
   def main(args: Array[String]) = println("Hello World! ai mae")
 =======
   def main(args: Array[String]) = println("Hello World! ai mae")
 >>>>>>> Olá a seguir ao Hello World
+=======
+  def main(args: Array[String]) = println("Hello World! Pls don't kill me\n")
+>>>>>>> parent of 76477c4... By Gello
 }
