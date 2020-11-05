4ea4b253c625bc592bd122a9140670e6b7e2e0cc
Syhko <syhko@outlook.com>
2020-11-05 15:50:00 +0000

    ai mae

--- src/teste/primeiro.scala
+++ src/teste/primeiro.scala
@@ -8,6 +8,6 @@ object Hello{
 <<<<<<< HEAD
   def main(args: Array[String]) = println("Hello World! Pls don't kill me\n\n\n")
 =======
-  def main(args: Array[String]) = println("Hello World! i dont even loooooooool")
+  def main(args: Array[String]) = println("Hello World! ai mae")
 >>>>>>> Olá a seguir ao Hello World
 }
