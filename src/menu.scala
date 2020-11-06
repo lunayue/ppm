7f8893f331592504f1dfa0679bf53a93f8df5f6e
AngeloNNGGAA <amendona6@gmail.com>
2020-11-06 13:06:08 +0000

    By Gello

--- src/menu.scala
+++ src/menu.scala
@@ -1,24 +1,32 @@
 import ioUtils._
 import session._
-//Bruh
+
 object menu extends App{
+
+  loginLoop()
+
   def loginLoop(): Unit ={
     val input = getUserInput("(l)ogin, (c)reate user, (q)uit:")
-    input match{
+    val user = input match{
       case "L" => login() //case None => loginLoop()
       case "C" => createUser() //case None => loginLoop()
       case "Q" => exit()
       case _ => println("Invalid Input"); loginLoop()
     }
+
+    user match {
+      case None => println("Comeback again")
+      case _ => mainLoop("Oq q é suposto meter aqui? O nome do User?")
+    }
+
   }
-  loginLoop()
 
   def mainLoop(u: String): Unit = {
 
     //criar um objecto como o prof fez para se poder fazer um loop em vez de um long String
     val input = getUserInput("1)perfil, 2)tracker, 3)qizzes, 4)compras, 5)alimentacao, 6)outros perfis, 7)logout")
 
-    input match{
+    input.toInt match{
       case 1 => println("perfil"); mainLoop(u)
 
       case 2 => println("tracker"); mainLoop(u) //bons e maus,
@@ -39,11 +47,12 @@ object menu extends App{
       case 6 => println("outros perfis"); mainLoop(u)
         // ir buscar os outros ficheiros de user e apresentar os valores q fazem sentido, talvez tentar fazer algun sorting
 
-      case 7 => println("logout"); logout()
+      case 7 => println("logout"); logout();loginLoop();
         // largar o user e voltar para o loginLoop???
+        // Gello: I think yes
 
       case _ => println("Invalid Input"); mainLoop(u)
     }
   }
-  mainLoop("ola")
+  //mainLoop("ola")
 }
--- src/session.scala
+++ src/session.scala
@@ -1,27 +1,30 @@
 import ioUtils._
 
 object session {
-  def login() = {
+  def login():Option[User] = {
     val username = getUserInputSensitive("Username")
     val password = getUserInputSensitive("Password")
     //ver se há ficheiro com o mmo username, se sim ver se a pass é igual
-    //manda Some(User)
+    Some(User(username,password)) //Dps da validação
   }
 
-  def createUser() = {
+  def createUser():Option[User] = {
     val username = getUserInputSensitive("Username")
     val password = getUserInputSensitive("Password")
     val repeticao = getUserInputSensitive("Repetir Password")
     //ver se há ficheiro com o mmo username, se ñ e as passes forem iguais cria o ficheiro
-    //manda Some(User)
+    Some(User(username,password)) //Dps da validação
   }
 
-  def logout() ={
+  def logout():Option[User] ={
     //save para file???
+    None //Dps da validação
   }
 
-  def exit() ={
+  def exit():Option[User] ={
     println("Adeus!")
+    None //Dps da validação (Faz sentido returnar None quando o User quer sair?)
+
   }
 
 
